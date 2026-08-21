b0cb60aa313c18b540c6470069c9261acdc9258c
Arsenii Hapechkin <Solomonlol95@gmail.com>
2026-08-21 15:26:13 +0300

    fix

--- Solomonlol.ShiftLogger/Interfaces/IEmployeeService.cs
+++ Solomonlol.ShiftLogger/Interfaces/IEmployeeService.cs
@@ -6,7 +6,7 @@ namespace ShiftLogger.Backend.Interfaces
     {
         Task<EmployeeDto?> GetByNumber(int employeeNumber, CancellationToken cancellationToken = default);
         Task<IEnumerable<EmployeeDto>> GetAll(CancellationToken cancellationToken = default);
-        Task<bool> Delete(int id, CancellationToken cancellationToken = default);
+        Task<bool> Delete(int employeeNumber, CancellationToken cancellationToken = default);
         Task<bool> Update(int employeeNumber, EmployeeDto item, CancellationToken cancellationToken = default);
         Task<bool> Create(EmployeeDto item, CancellationToken cancellationToken = default);
     }
